bc0a0036d6ede7d2fbc5265986c6df963584c5d5
niyazi <niyazi_1992@abv.bg>
2020-08-22 22:12:15 +0300

    dynamis-fields

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,8 +1,13 @@
 $(function () {
 
+    //Green message functionality
+    setTimeout(function () {
+        $('.message').slideUp();
+    }, 4000);
+
     $('#tel-numbers > #add-tel').click(function () {
-        var countNumbers = $("span[id^='error-telephone_number_new']").length;
-        var i = 1 === countNumbers ? 1 : countNumbers;
+        let countNumbers = $("span[id^='error-telephone_number_new']").length;
+        let i = 1 === countNumbers ? 1 : countNumbers;
         $('#tel-numbers').append('<div class="form-group"><div class="d-flex"><input type="number" name="telephone_number_new[]" placeholder="Enter telephone number" class="form-control mr-1" /><button title="remove" type="button" class="remove-tel btn btn-danger"><i class="fa fa-trash"></i></button></div><span id="error-telephone_number_new.' + i++ + '" class="invalid-feedback"></span></div>');
     });
 
@@ -15,7 +20,7 @@ $(function () {
 
     $('#contact-form').submit(function (e) {
         e.preventDefault();
-        var form = $(this);
+        let form = $(this);
         $.ajax({
             processData: false,
             contentType: false,
@@ -26,18 +31,17 @@ $(function () {
                 window.location = data.route;
             },
             error: function (xhr) {
-                var errors = xhr.responseJSON.errors;
+                let errors = xhr.responseJSON.errors;
                 $("span[id^='error-']").text('');
                 $("#contact-form :input").removeClass('is-invalid');
                 $("#contact-form :input").addClass('is-valid');
                 $.each(errors, function (i, item) {
-                    var t = i.replace('.', '\\.');
+                    let t = i.replace('.', '\\.');
                     $('#error-' + t).parent().find('input:first, select:first').addClass('is-invalid');
                     $('#error-' + t).html(item);
                 });
             },
-
         });
     });
 
-});  
+});
